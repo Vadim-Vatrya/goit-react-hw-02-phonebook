5f060925f1eae4e90a9d406a616deb4b071f2d98
Vadim-Vatrya <VatryaVV@gmamil.com>
2021-02-28 07:36:53 +0200

    task 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import ContactForm from './components/ContactForm/ContactForm';
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  addContact = ({name, number}) => {
+    const {contacts} = this.state;
+
+    if (name === '') {
+      alert(`Enter your name`)
+    };
+
+    
+    if (number === '') {
+      alert(`Enter your number`)
+    };
+
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`)
+    };
+
+    const newContact = {
+      id: uuidv4,
+      name,
+      number,
+    }
+
+    this.setState(({contacts}) => ({contacts: [newContact, ...contacts]}))
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact}/>
+        <h2>Contacts</h2>
+        <Filter />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import { v4 as uuidv4 } from 'uuid';
+import Button from '../Button/Button'
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+handleChange = event => {
+  const {name, value} = event.currentTarget;
+  this.setState({[name]: value});
+}
+
+  handleSubmit = event  => {
+    const {name, number} = this.state;
+    event.preventDefault();
+    this.props.onSubmit(name, number);
+    this.reset()
+  }
+
+  reset () {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  }
+
+
+  contactNameId = uuidv4();
+  contactNumberId = uuidv4();
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor={this.contactNameId}>
+          <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+          id ={this.contactNameId}
+          ></input>
+        </label>
+        <label htmlFor={this.contactNumberId}>
+          <input
+          type="tel"
+          name="number"
+          value={this.state.number}
+          onChange={this.handleChange}
+          id={this.contactNumberId}
+          ></input>
+        </label>
+        <Button />
+      </form>
+      
+    )
+  }
+}
+
+export default ContactForm;
